1a6417e6e20b9dc533c003749b60fc55577e7a0d
Tich <81482907+tichanna@users.noreply.github.com>
2024-11-05 19:41:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,6 @@ dump(scaler, 'scaler.joblib')
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile app.py
- import pandas as pd
- import numpy as np
  import streamlit as st
  from sklearn.ensemble import RandomForestRegressor
  from sklearn.preprocessing import StandardScaler
